5b53904eec165886806984883e8731f0244a120f
StefanoTavonatti <stefanotavonatti93@gmail.com>
2020-04-22 14:24:18 +0200

    removed migrations

--- docker-support/run.sh
+++ docker-support/run.sh
@@ -18,21 +18,5 @@ if [ -z "${WORKERS}" ]; then
     WORKERS=$DEFAULT_WORKERS
 fi
 
-echo "Creating manage script for repository"
-
-migrate manage manage.py --repository=migrations --url=$DB_CONNECTION_STRING
-
-echo "Initialize version control of the database"
-
-python manage.py db_version >/dev/null 2>&1
-if [ $? != 0 ]; then
-  python manage.py version_control
-else
-  echo " |_ Database already initialized"
-fi
-
-echo "Perform migrations"
-python manage.py upgrade
-
 echo "Running ws"
 exec gunicorn -w "${WORKERS}" -b 0.0.0.0:80 "wenet.wenet_service_api.main:wenet_service_api"
