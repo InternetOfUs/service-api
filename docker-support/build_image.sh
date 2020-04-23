63052cb3a7bde174234655bcb928ef9a21591bce
StefanoTavonatti <stefanotavonatti93@gmail.com>
2020-04-23 15:54:10 +0200

    Edit build script and added gitlab-ci file

--- docker-support/build_image.sh
+++ docker-support/build_image.sh
@@ -15,14 +15,6 @@ PROJECT_DIR=${SCRIPT_DIR}/..
 
 # Identifying build version
 
-VERSION=$1
-if [ -z "${VERSION}" ]; then
-    VERSION=${DEFAULT_VERSION}
-    echo "Build version not specified: building with default version [${VERSION}]"
-else
-    echo "Building with specified version [${VERSION}]"
-fi
-
 mkdir ${SCRIPT_DIR}/src
 mkdir ${SCRIPT_DIR}/tests
 mkdir ${SCRIPT_DIR}/docs
@@ -36,24 +28,10 @@ cp -R ${PROJECT_DIR}/migrations/* ${SCRIPT_DIR}/migrations
 
 # Building image
 
-IMAGE_NAME=wenet/service-api:${VERSION}
 docker build -t ${IMAGE_NAME} ${SCRIPT_DIR}
 if [ $? == 0 ]; then
 
     echo "Build successful: ${IMAGE_NAME}"
-
-    # Running tests
-
-    if [[ "${SKIP_TESTS}" -eq "1" ]]; then
-        echo "WARN: Forcing test skipping"
-    else
-        docker run -it --rm ${IMAGE_NAME} ./run_tests.sh
-        if [ $? != 0 ]; then
-            echo "ERR: One or more tests are failing"
-            clean
-            exit 1
-        fi
-    fi
 
     # Tagging images for registry
 
---
+++ docker-support/runner.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+DEFAULT_VERSION="1.3.0"
+
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+BUILD=0
+TEST=0
+DELETE_IF_FAILED=0
+SAVE_IMAGE_TO_TARGZ=0
+PUSH=0
+
+args=`getopt btdsp $*`
+# you should not use `getopt abo: "$@"` since that would parse
+# the arguments differently from what the set command below does.
+if [ $? != 0 ]
+then
+  echo 'Usage: ...'
+  exit 2
+fi
+set -- $args
+# You cannot use the set command with a backquoted getopt directly,
+# since the exit code from getopt would be shadowed by those of set,
+# which is zero by definition.
+for i do
+  case "$i" in
+     -b)
+       BUILD=1
+       shift;;
+     -t)
+       TEST=1
+       shift;;
+     -d)
+       DELETE_IF_FAILED=1
+       shift;;
+     -s)
+       SAVE_IMAGE_TO_TARGZ=1
+       shift;;
+     -p)
+       PUSH_IMAGE=1
+       shift;;
+  esac
+done
+
+# Identifying build version
+VERSION=$2
+if [ -z "${VERSION}" ]; then
+    VERSION=${DEFAULT_VERSION}
+    echo "Version not specified: building with default version [${VERSION}]"
+else
+    echo "Using specified version [${VERSION}]"
+fi
+
+# Exporting image name for the build and test script
+export IMAGE_NAME=wenet/service-api:${VERSION}
+
+
+if [ $BUILD == 1 ]; then
+  echo "Building chatbot-core image"
+  ${SCRIPT_DIR}/build_image.sh
+
+  if [ $? != 0 ]; then
+    echo "Build Failed"
+    exit 1
+  fi
+
+  if [ $SAVE_IMAGE_TO_TARGZ == 1 ]; then
+    echo "Saving image to service_api_image.tar.gz"
+    docker save ${IMAGE_NAME} | gzip > service_api_image.tar.gz
+  fi
+fi
+
+if [ $TEST == 1 ]; then
+  echo "Running test on chatbot-core image"
+  ${SCRIPT_DIR}/test.sh
+
+  if [ $? != 0 ]; then
+      echo "Error: One or more tests are failing"
+      if [ $DELETE_IF_FAILED == 1 ]; then
+        docker rmi ${IMAGE_NAME}
+      fi
+      exit 1
+  fi
+
+fi
+
+if [ $PUSH == 1]; then
+  echo "Pushing image to registry"
+  docker push ${IMAGE_NAME}
+fi
+
+if [ $BUILD == 0 ] && [ $TEST == 0 ]; then
+  echo "Need to specify at least one parameter (-b or -t)"
+  exit 1
+fi
+
+
