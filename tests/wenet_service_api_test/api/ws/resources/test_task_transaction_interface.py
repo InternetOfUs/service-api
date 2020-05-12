1a7c1a38ae3f58163e9a162050e07a248474703d
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2020-05-12 12:31:02 +0200

    Updated TaskTransactionInterface unit tests

--- tests/wenet_service_api_test/api/ws/resources/test_task_transaction_interface.py
+++ tests/wenet_service_api_test/api/ws/resources/test_task_transaction_interface.py
@@ -8,14 +8,14 @@ class TestTaskTransactionInterface(CommonTestCase):
 
     def test_post(self):
 
-        task_transaction = TaskTransaction("taskId", TaskTransaction.LABEL_ACCEPT_VOLUNTEER, {"key": "value"})
+        task_transaction = TaskTransaction("taskId", "label", {"key": "value"})
 
         response = self.client.post("/task/transaction", json=task_transaction.to_repr(), headers={"apikey": self.AUTHORIZED_APIKEY})
         self.assertEqual(response.status_code, 201)
 
     def test_post_not_authenticated(self):
 
-        task_transaction = TaskTransaction("taskId", TaskTransaction.LABEL_ACCEPT_VOLUNTEER, {"key": "value"})
+        task_transaction = TaskTransaction("taskId", "label", {"key": "value"})
 
         response = self.client.post("/task/transaction", json=task_transaction.to_repr())
         self.assertEqual(response.status_code, 403)
