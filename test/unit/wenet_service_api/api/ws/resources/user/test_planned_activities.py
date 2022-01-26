f0715fcc8a34704db01eaa7fdc275ae2b80d6575
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2022-01-26 17:30:35 +0100

    Updated planned activities endpoint in order to use the new endpoints. Issue #61

--- test/unit/wenet_service_api/api/ws/resources/user/test_planned_activities.py
+++ test/unit/wenet_service_api/api/ws/resources/user/test_planned_activities.py
@@ -155,7 +155,7 @@ class TestWeNetUserPlannedActivitiesInterface(CommonTestCase):
         response = self.client.get("/user/profile/1/plannedActivities", headers={
             "apikey": self.AUTHORIZED_APIKEY,
             "x-wenet-source": WenetSource.OAUTH2_AUTHORIZATION_CODE.value,
-            "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value}",  # TODO add reading scope when will be added
+            "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value} {Scope.ACTIVITIES_READ.value}",
             "X-Authenticated-Userid": profile_id,
             "X-Consumer-Username": "app_1"
         })
@@ -165,7 +165,71 @@ class TestWeNetUserPlannedActivitiesInterface(CommonTestCase):
 
         mock_get.assert_called_once()
         self.service_collector_connector.hub_connector.get_app_details.assert_called_once()
-        self.service_collector_connector.hub_connector.get_user_ids_for_app.assert_called_once()
+        self.service_collector_connector.hub_connector.get_user_ids_for_app.assert_not_called()
+
+    def test_get_oauth_missing_scope(self):
+        profile_id = "1"
+        profile = WeNetUserProfile(
+            name=UserName(
+                first="first",
+                middle="middle",
+                last="last",
+                prefix="prefix",
+                suffix="suffix"
+            ),
+            date_of_birth=Date(
+                year=2020,
+                month=1,
+                day=20
+            ),
+            gender=Gender.MALE,
+            email="email@example.com",
+            phone_number="phone number",
+            locale="it_IT",
+            avatar="avatar",
+            nationality="it",
+            occupation="occupation",
+            creation_ts=1579536160,
+            last_update_ts=1579536160,
+            profile_id=profile_id,
+            norms=[],
+            planned_activities=[
+                {
+                    "id": "hfdsfs888",
+                    "startTime": {},
+                    "endTime": {},
+                    "description": "A few beers for relaxing",
+                    "attendees": [
+                        "15d85f1d-b1ce-48de-b221-bec9ae954a88"
+                    ],
+                    "status": "confirmed"
+                }
+            ],
+            relevant_locations=[],
+            relationships=[],
+            personal_behaviours=[],
+            materials=[],
+            competences=[],
+            meanings=[]
+        )
+
+        mock_get = Mock(return_value=deepcopy(profile))
+        self.service_collector_connector.hub_connector.get_app_details = Mock(return_value=self.app)
+        self.service_collector_connector.profile_manager_collector.get_user_profile = mock_get
+        self.service_collector_connector.hub_connector.get_user_ids_for_app = Mock(return_value=self.user_list)
+
+        response = self.client.get("/user/profile/1/plannedActivities", headers={
+            "apikey": self.AUTHORIZED_APIKEY,
+            "x-wenet-source": WenetSource.OAUTH2_AUTHORIZATION_CODE.value,
+            "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value}",
+            "X-Authenticated-Userid": profile_id,
+            "X-Consumer-Username": "app_1"
+        })
+        self.assertEqual(403, response.status_code)
+
+        mock_get.assert_not_called()
+        self.service_collector_connector.hub_connector.get_app_details.assert_called_once()
+        self.service_collector_connector.hub_connector.get_user_ids_for_app.assert_not_called()
 
     def test_get_oauth_2(self):
         profile_id = "2"
@@ -289,7 +353,7 @@ class TestWeNetUserPlannedActivitiesInterface(CommonTestCase):
         response = self.client.put(f"/user/profile/{profile_id}/plannedActivities", json=planned_activities, headers={
             "apikey": self.AUTHORIZED_APIKEY,
             "x-wenet-source": WenetSource.OAUTH2_AUTHORIZATION_CODE.value,
-            "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value}",  # TODO add writing scope when will be added
+            "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value} {Scope.ACTIVITIES_WRITE.value} {Scope.ACTIVITIES_READ.value}",
             "X-Authenticated-Userid": profile_id,
             "X-Consumer-Username": "app_1"
         })
@@ -298,6 +362,79 @@ class TestWeNetUserPlannedActivitiesInterface(CommonTestCase):
         self.assertEqual(planned_activities, json_response)
 
         mock_patch.assert_called_once()
+        self.service_collector_connector.hub_connector.get_app_details.assert_called_once()
+        self.service_collector_connector.hub_connector.get_app_developers.assert_called_once()
+
+    def test_put_oauth_no_read_permission(self):
+        profile_id = "1"
+        planned_activities = [
+            {
+                "id": "hfdsfs888",
+                "startTime": {},
+                "endTime": {},
+                "description": "A few beers for relaxing",
+                "attendees": [
+                    "15d85f1d-b1ce-48de-b221-bec9ae954a88"
+                ],
+                "status": "confirmed"
+            }
+        ]
+
+        patched_profile = PatchWeNetUserProfile(profile_id=profile_id, planned_activities=planned_activities)
+
+        mock_patch = Mock(return_value=patched_profile)
+        self.service_collector_connector.profile_manager_collector.patch_user_profile = mock_patch
+        self.service_collector_connector.hub_connector.get_app_details = Mock(return_value=self.app1)
+        self.service_collector_connector.hub_connector.get_app_developers = Mock(return_value=self.developer_list)
+
+        response = self.client.put(f"/user/profile/{profile_id}/plannedActivities", json=planned_activities,
+                                   headers={
+                                       "apikey": self.AUTHORIZED_APIKEY,
+                                       "x-wenet-source": WenetSource.OAUTH2_AUTHORIZATION_CODE.value,
+                                       "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value} {Scope.ACTIVITIES_WRITE.value}",
+                                       "X-Authenticated-Userid": profile_id,
+                                       "X-Consumer-Username": "app_1"
+                                   })
+        self.assertEqual(200, response.status_code)
+        json_response = json.loads(response.data)
+        self.assertEqual([], json_response)
+
+        mock_patch.assert_called_once()
+        self.service_collector_connector.hub_connector.get_app_details.assert_called_once()
+        self.service_collector_connector.hub_connector.get_app_developers.assert_called_once()
+
+    def test_put_oauth_not_authorized(self):
+        profile_id = "1"
+        planned_activities = [
+            {
+                "id": "hfdsfs888",
+                "startTime": {},
+                "endTime": {},
+                "description": "A few beers for relaxing",
+                "attendees": [
+                    "15d85f1d-b1ce-48de-b221-bec9ae954a88"
+                ],
+                "status": "confirmed"
+            }
+        ]
+
+        patched_profile = PatchWeNetUserProfile(profile_id=profile_id, planned_activities=planned_activities)
+
+        mock_patch = Mock(return_value=patched_profile)
+        self.service_collector_connector.profile_manager_collector.patch_user_profile = mock_patch
+        self.service_collector_connector.hub_connector.get_app_details = Mock(return_value=self.app1)
+        self.service_collector_connector.hub_connector.get_app_developers = Mock(return_value=self.developer_list)
+
+        response = self.client.put(f"/user/profile/{profile_id}/plannedActivities", json=planned_activities, headers={
+            "apikey": self.AUTHORIZED_APIKEY,
+            "x-wenet-source": WenetSource.OAUTH2_AUTHORIZATION_CODE.value,
+            "X-Authenticated-Scope": f"{Scope.ID_READ.value} {Scope.FIRST_NAME_READ.value} {Scope.ACTIVITIES_READ.value}",
+            "X-Authenticated-Userid": profile_id,
+            "X-Consumer-Username": "app_1"
+        })
+        self.assertEqual(403, response.status_code)
+
+        mock_patch.assert_not_called()
         self.service_collector_connector.hub_connector.get_app_details.assert_called_once()
         self.service_collector_connector.hub_connector.get_app_developers.assert_called_once()
 
